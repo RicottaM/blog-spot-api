462f3407b9a802903b09e87353a93bdb3ad137ae
RicottaM <jaskulskim.gdynia@gmail.com>
2025-09-25 00:27:12 +0200

    Fixed no dbservice in post-comments

--- src/api/post-comments/post-comments.module.ts
+++ src/api/post-comments/post-comments.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostCommentsService } from './post-comments.service';
 import { PostCommentsController } from './post-comments.controller';
+import { DatabaseService } from '../database/database.service';
 
 @Module({
   controllers: [PostCommentsController],
-  providers: [PostCommentsService],
+  providers: [PostCommentsService, DatabaseService],
 })
 export class PostCommentsModule {}
